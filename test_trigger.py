a39bebd8faca2e4458f0b50b63d7010eea0ed421
Ilkay Isik <ilkayisik@gmail.com>
2017-09-25 12:02:09 +0200

    updates after testing in the lab

--- test_trigger.py
+++ test_trigger.py
@@ -1,8 +1,9 @@
 '''Test the trigger via the parallel port'''
 
 from psychopy import visual, parallel, core, logging, event
+
 win = visual.Window(size=(800,600),
-                    allowGUI=True, 
+                    allowGUI=True,
                     color='grey')
 
 parallel.setPortAddress(address=0xCFE9)
@@ -17,11 +18,22 @@ start_trigger = True
 while start_trigger:
     text.draw()
     win.flip()
-
+# test the trigger with the parallel  port
     if parallel.readPin(13) == 1:
-            start_trigger = False
+        # start_trigger = False
+        text.text = 'I got the trigger'
+        text.draw()
+        win.flip()
+
+        for keys in event.getKeys():
+
+                if keys[0] in ['escape', 'q']:
+                    win.close()
+                    core.quit()
+
+#test the triger with keyboard press
     for keys in event.getKeys():
- 
+
         if keys in ['5']:
             start_trigger = False
 
@@ -30,5 +42,4 @@ while start_trigger:
             core.quit()
 
 globalClock.reset()
-logging.exp('parallel trigger: start of scan')
-win.flip()  # blank the screen on first sync pulse received
+#win.flip()  # blank the screen on first sync pulse received
