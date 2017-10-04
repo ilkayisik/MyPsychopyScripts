9868e134f064d868da3a068cbf3a39b75d673334
Ilkay Isik <ilkayisik@gmail.com>
2017-10-04 20:46:31 +0200

    commit

--- movie_simple.py
+++ movie_simple.py
@@ -3,11 +3,11 @@ from psychopy import visual, core, event
 import os
 
 win = visual.Window((1920, 1080))
-mov = visual.MovieStim3(win, '/Users/ilkay.isik/Desktop/Matched_Stim/d_12.mp4', 
-    size=(1280, 720),
-    flipVert=False, 
-    flipHoriz=False, 
-    loop=False)
+mov = visual.MovieStim3(win, '/Volumes/Projekte/2016-0065-vesfmri1/Stimuli/Orig_movies/BluerayMovies/SAMSARA/Samsara_1280x720.m4v', #'/Volumes/Projekte/2016-0065-vesfmri1/Movie_Pilot/Beh_Pilot_04/stimuli/d_01.mp4', 
+size=(1280, 720),
+flipVert=False, 
+flipHoriz=False,
+loop=False)
 
 
 print('orig movie size=%s' % mov.size)
