d0440badda251967f3d952937d1a758f31a14d0f
Ilkay Isik <ilkayisik@gmail.com>
2017-10-07 18:24:26 +0200

    if condition to be able to run on mac

--- test_trigger.py
+++ test_trigger.py
@@ -1,11 +1,19 @@
 '''Wait the trigger using parallel port input or keyboard press.
-Use the response box from Cornelius to emulate signal'''
+
+Use the response box from Cornelius to emulate signal.
+'''
 
 from psychopy import visual, parallel, core, event
-from psychopy.parallel._inpout32 import PParallelInpOut32
 
-port = PParallelInpOut32()
-pin, expected_trigger = 13, 0
+# Does the computer have a parallel port (True or False)
+parallel_port = False
+
+if parallel_port:  # this solution is for windows machines
+    from psychopy.parallel._inpout32 import PParallelInpOut32
+    port = PParallelInpOut32()
+    # Parallel port related parameters
+    pin = 13
+    expected_trigger = 0  # can be 0 or 1
 
 # Prepare the window and the stimuli
 win = visual.Window(size=(800,600), allowGUI=True, color='grey')
@@ -17,7 +25,8 @@ global_clock = core.Clock()
 while start_trigger:
     text.draw()
     win.flip()
-    if port.readPin(pin) == expected_trigger:
+
+    if parallel_port and port.readPin(pin) == expected_trigger:
         start_trigger = False
 
     for keys in event.getKeys():
