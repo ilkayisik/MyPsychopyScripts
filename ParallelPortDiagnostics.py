2cc773c3d4cf8ceded8d02d93e78a55e78971e44
Ilkay Isik <ilkayisik@gmail.com>
2017-10-07 18:05:25 +0200

    fix typo

--- ParallelPortDiagnostics.py
+++ ParallelPortDiagnostics.py
@@ -18,7 +18,7 @@ def ReadPins():
     out = '|'
     selected_pins = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
     for pin in selected_pins:
-        out += str(port.readPin(pin))  + '|' #read the pin value - 0 or 1  
+        out += str(port.readPin(pin))  + '|' #read the pin value - 0 or 1
     return out
 
 
@@ -27,14 +27,14 @@ loop_switch = True
 while loop_switch:
     text_1.text = ReadPins()
     text_1.draw()
-    text_2.text = '8-bit data: ' + str(port.readData())  #read the combined data from the input pins 
+    text_2.text = '8-bit data: ' + str(port.readData())  #read the combined data from the input pins
     text_2.draw()
     win.flip()
     for keys in event.getKeys():
         if keys[0] in ['escape', 'q']:  # quit
             loop_switch = False
         elif keys[0] in ['2']:
-            port.setPin(2, (port.readPin(2) + 1) % 2)# add one each time the button is pressed 
+            port.setPin(2, (port.readPin(2) + 1) % 2)# add one each time the button is pressed
         elif keys[0] in ['3']:
             port.setPin(3, (port.readPin(3) + 1) % 2)
         elif keys[0] in ['4']:
@@ -48,5 +48,4 @@ while loop_switch:
         elif keys[0] in ['8']:
             port.setPin(8, (port.readPin(8) + 1) % 2)
         elif keys[0] in ['9']:
-            port.setPin(9 (port.readPin(9) + 1) % 2)
-
+            port.setPin(9,(port.readPin(9) + 1) % 2)
